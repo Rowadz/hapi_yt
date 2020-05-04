cc476ba01eca831ccfbf9878748a5db44539439b
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-05 01:50:28 +0300

    adding types

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
 import * as Hapi from '@hapi/hapi';
+import { Server, ResponseToolkit } from 'hapi';
 
 const init = async () => {
-  const server = Hapi.server({
+  const server: Server = Hapi.server({
     port: 3000,
     host: 'localhost',
   });
 
+  // request - the request object.
+  // h - the response toolkit the handler must call to set a response and return control back to the framework.
+  // err - an error object availble only when the method is used as a failAction value.
   server.route({
     method: 'GET',
     path: '/',
-    handler: (request, h) => {
+    handler: (request: Request, h: ResponseToolkit, err?: Error) => {
       return { msg: 'hello world' };
     },
   });
