ea481399bda3ce8c68dc638e38d42744392e8e17
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-05 01:41:03 +0300

    convert index.js to ts

---
+++ index.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+import * as Hapi from '@hapi/hapi';
+import { Server } from 'hapi';
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return { msg: 'hello world' };
+    },
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
