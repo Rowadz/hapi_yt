1cac98a50e4fb1d78826aa02eea685332ba10dad
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-07 23:06:48 +0300

    adding the rntity

--- db/con.ts
+++ db/con.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import { Connection, createConnection } from 'typeorm';
+import { UsersEntity } from './entities';
 
 export const initDb = async (): Promise<Connection> => {
   const con = await createConnection({
     type: 'sqlite',
     // this will be created relatvie to the path where you call this function
     database: './hapi.db',
+    entities: [UsersEntity],
   });
   await con.synchronize(true);
   return con;
