bee3a60b077c2c08010f8bb96c56fd2d2cc9f934
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-07 23:53:23 +0300

    arr of fake funcs

--- db/con.ts
+++ db/con.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import { Connection, createConnection } from 'typeorm';
 import { UsersEntity, PostsEntity } from './entities';
-import { fakeUsers } from './fakingDate';
+import { fakeUsers, fakePosts } from './fakingDate';
 import 'colors';
 
 export const initDb = async (): Promise<Connection> => {
   const entities = [UsersEntity, PostsEntity];
+  const fakeFuncs = [fakeUsers, fakePosts];
   const con = await createConnection({
     type: 'sqlite',
     // this will be created relatvie to the path where you call this function
@@ -15,6 +16,6 @@ export const initDb = async (): Promise<Connection> => {
   await con.synchronize(true);
   entities.forEach((entity) => console.log(`Created ${entity.name}`.blue));
   console.log('Creating fake Data...'.yellow.bold);
-  await fakeUsers(con);
+  for (const fun of fakeFuncs) await fun(con);
   return con;
 };
