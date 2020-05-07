07d07df64f666b0a00f91a66bb8858b37e00ad44
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-07 23:12:13 +0300

    logging

--- db/con.ts
+++ db/con.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
 import { Connection, createConnection } from 'typeorm';
 import { UsersEntity } from './entities';
+import 'colors';
 
 export const initDb = async (): Promise<Connection> => {
+  const entities = [UsersEntity];
   const con = await createConnection({
     type: 'sqlite',
     // this will be created relatvie to the path where you call this function
     database: './hapi.db',
-    entities: [UsersEntity],
+    entities,
   });
   await con.synchronize(true);
+  entities.forEach((entity) => console.log(`Created ${entity.name}`.blue));
   return con;
 };
