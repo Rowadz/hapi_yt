7271c17470bae2877e210efbbd2918162f28dd94
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-07 23:01:12 +0300

    users entity

---
+++ db/entities/users.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UsersEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'first_name', nullable: false })
+  firstName: string;
+
+  @Column({ name: 'last_name', nullable: false })
+  lastName: string;
+
+  @Column({ name: 'birth_of_date', nullable: true })
+  birthOfDate: Date;
+
+  @Column({ default: 'user' })
+  type: 'admin' | 'user';
+}
+
+/**
+ * for the `type` use enum like this in mysql or postgres
+ * enum UserType {
+ *      user = 'user',
+ *      admin = 'admin
+ * }
+ * @Column({ default: UserType.user, enum: UserType, type: 'enum' })
+ * type: UserType;
+ *
+ */
