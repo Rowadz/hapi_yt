5b8a6de4ea6bf1e0690573e15b6bf9d2023e1859
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-07 23:19:22 +0300

    adding the entity to con

--- db/con.ts
+++ db/con.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
 import { Connection, createConnection } from 'typeorm';
-import { UsersEntity } from './entities';
+import { UsersEntity, PostsEntity } from './entities';
 import 'colors';
 
 export const initDb = async (): Promise<Connection> => {
-  const entities = [UsersEntity];
+  const entities = [UsersEntity, PostsEntity];
   const con = await createConnection({
     type: 'sqlite',
     // this will be created relatvie to the path where you call this function
