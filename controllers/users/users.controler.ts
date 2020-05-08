bdc785f63b3da40884caf7a7dcd3a0864bb7bb0d
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-08 22:37:52 +0300

    basic crud

--- controllers/users/users.controler.ts
+++ controllers/users/users.controler.ts
@@ -1,15 +1,66 @@
 import { Connection, Repository } from 'typeorm';
 import { UsersEntity } from '../../db/entities';
-import { ResponseToolkit } from 'hapi';
+import { ResponseToolkit, ServerRoute, Request } from 'hapi';
 
-export const UserController = (con: Connection) => {
+export const UserController = (con: Connection): Array<ServerRoute> => {
   const userRepo: Repository<UsersEntity> = con.getRepository(UsersEntity);
-  return {
-    get: {
+  return [
+    {
       method: 'GET',
-      path: '/',
-      handler: async (request: Request, h: ResponseToolkit, err?: Error) =>
+      path: '/users',
+      handler: (request: Request, h: ResponseToolkit, err?: Error) =>
         userRepo.find(),
     },
-  };
+    {
+      method: 'GET',
+      path: '/users/{id}',
+      handler: ({ params: { id } }: Request, h: ResponseToolkit, err?: Error) =>
+        userRepo.findOne(id),
+    },
+    {
+      method: 'POST',
+      path: '/users',
+      handler: ({ payload }: Request, h: ResponseToolkit, err?: Error) => {
+        // TODO:: anyone can select the type?? no!
+        const { firstName, lastName, birthOfDate, email } = payload as Partial<
+          UsersEntity
+        >;
+
+        const u: Partial<UsersEntity> = new UsersEntity(
+          firstName,
+          lastName,
+          email,
+          birthOfDate
+        );
+        return userRepo.save<Partial<UsersEntity>>(u);
+      },
+    },
+    {
+      method: 'PATCH',
+      path: '/users/{id}',
+      handler: async (
+        { params: { id }, payload }: Request,
+        h: ResponseToolkit,
+        err?: Error
+      ) => {
+        const u = await userRepo.findOne(id);
+        Object.keys(payload).forEach((key) => (u[key] = payload[key]));
+        userRepo.update(id, u);
+        return u;
+      },
+    },
+    {
+      method: 'DELETE',
+      path: '/users/{id}',
+      handler: async (
+        { params: { id } }: Request,
+        h: ResponseToolkit,
+        err?: Error
+      ) => {
+        const u = await userRepo.findOne(id);
+        userRepo.remove(u);
+        return u;
+      },
+    },
+  ];
 };
