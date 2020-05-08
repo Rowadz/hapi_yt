30a9a0537a563dd58a27321eb8101ac6b9c1b823
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-09 01:53:23 +0300

    good query and pagination ?

--- controllers/users/users.controler.ts
+++ controllers/users/users.controler.ts
@@ -10,22 +10,39 @@ export const userController = (con: Connection): Array<ServerRoute> => {
       method: 'GET',
       path: '/users',
       handler: async ({ query }: Request, h: ResponseToolkit, err?: Error) => {
-        const { perPage, page } = query;
+        let { perPage, page, ...q } = query;
         let realPage: number;
         let realTake: number;
         if (perPage) realTake = +perPage;
-        else realTake = 10;
+        else {
+          perPage = '10';
+          realTake = 10;
+        }
         if (page) realPage = +page === 1 ? 0 : (+page - 1) * realTake;
-        else realPage = 0;
-
+        else {
+          realPage = 0;
+          page = '1';
+        }
+        const findOptions = {
+          take: realTake,
+          skip: realPage,
+          where: { ...q },
+        };
+        if (!q) delete findOptions.where;
+        const getQuery = () =>
+          Object.keys(q)
+            .map((key) => `${key}=${q[key]}`)
+            .join('&');
         return {
-          data: await userRepo.find({ take: realTake, skip: realPage }),
+          data: await userRepo.find(findOptions),
           perPage: realTake,
           page: +page || 1,
-          next: `http://localhost:3000/users?perPage=${realTake}&page=${page}`,
+          next: `http://localhost:3000/users?perPage=${realTake}&page=${
+            +page + 1
+          }&${getQuery()}`,
           prev: `http://localhost:3000/users?perPage=${realTake}&page=${
             +page - 1
-          }`,
+          }&${getQuery()}`,
         };
       },
     },
