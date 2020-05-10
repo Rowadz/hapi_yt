2901bf0431eda60a05aa8457682c180d4b6bd09c
MohammedAl-rowad <rowadwo@gmail.com>
2020-05-10 03:34:22 +0300

    basic auth validate func POC

--- auth.ts
+++ auth.ts
@@ -1,4 +1,10 @@
-export const validate = async function (decoded, request, h) {
+import { Request, ResponseToolkit } from '@hapi/hapi';
+
+export const validateJWT = async (
+  decoded: object,
+  request: Request,
+  h: ResponseToolkit
+) => {
   // do your checks to see if the person is valid
   console.log(decoded);
   //   if (!people[decoded.id]) {
@@ -6,4 +12,24 @@ export const validate = async function (decoded, request, h) {
   //   } else {
   //     return { isValid: true };
   //   }
+};
+
+export const validateBasic = async (
+  request: Request,
+  username: string,
+  password: string,
+  h: ResponseToolkit
+) => {
+  console.log(username, password);
+
+  // const user = users[username];
+  // if (!user) {
+  // return { credentials: null, isValid: false };
+  // }
+
+  // const isValid = await Bcrypt.compare(password, user.password);
+  // const credentials = { id: user.id, name: user.name };
+
+  // credentials - a credentials object passed back to the application in `request.auth.credentials`.
+  return { isValid: true, credentials: { nidce: 'true' } };
 };
